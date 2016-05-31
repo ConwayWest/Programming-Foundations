111d5c4d22ab66c63626ab15a31da02eb2a1ad6c
Kyle Conway <kyleconway@rocketmail.com>
2016-05-31 12:40:01 -0700

    Home Tuesday

--- Redo/lesson_4/twenty_one.rb
+++ Redo/lesson_4/twenty_one.rb
@@ -1,4 +1,4 @@
-DECK = [['S', '2'], ['S', '3'], ['S', '4'],
+deck = [['S', '2'], ['S', '3'], ['S', '4'],
         ['S', '5'], ['S', '6'], ['S', '7'],
         ['S', '8'], ['S', '9'], ['S', '10'],
         ['S', 'J'], ['S', 'Q'], ['S', 'K'],
@@ -15,4 +15,43 @@ DECK = [['S', '2'], ['S', '3'], ['S', '4'],
         ['C', '5'], ['C', '6'], ['C', '7'],
         ['C', '8'], ['C', '9'], ['C', '10'],
         ['C', 'J'], ['C', 'Q'], ['C', 'K'],
-        ['C', 'A']].freeze
+        ['C', 'A']]
+player_hand = []
+dealer_hand = []
+
+def ace_value(card)
+  if card + 11 > 21
+    1
+  else
+    11
+  end
+end
+
+def face_value(card)
+  if card == 'J' || card == 'Q' || card == 'K'
+    10
+  end
+end
+
+def shuffle(d)
+  d.shuffle!
+end
+
+def deal_cards(player, dealer, d)
+  2.times do 
+    player << d.sample
+    dealer << d.sample
+  end
+end
+
+loop do
+  shuffle(deck)
+  deal_cards(player_hand, dealer_hand, deck)
+  deck.delete(player_hand)
+  deck.delete(dealer_hand)
+
+  p player_hand
+  p dealer_hand
+  p deck
+  break
+end
