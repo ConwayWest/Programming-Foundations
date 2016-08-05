b50a7f06801e856954b5d592218b4c2e866241fd
Conway <kyle.conway@sykes.com>
2016-08-04 20:44:27 -0700

    Work Thur

---
+++ exercises/ruby_basics/loops_1/exercise_10.rb
@@ -0,0 +1,11 @@
+# friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+# for i in 0...friends.size
+#   puts "Hello, #{friends[i]}!"
+# end
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends
+  puts "Hello, #{friend}!"
+end
