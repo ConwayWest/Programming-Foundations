594cbf6be0b30e954547fb81f3845985e31ea75f
Kyle Conway <kyleconway@rocketmail.com>
2016-08-14 07:35:04 -0700

    Home Sun

---
+++ exercises/ruby_basics/methods/exercise_8.rb
@@ -0,0 +1,16 @@
+def sentence(name, activity)
+  "#{name} went #{activity} today!"
+end
+
+def name(name_array)
+  name_array.sample
+end
+
+def activity(activity_array)
+  activity_array.sample
+end
+
+names = ['Dave', 'Sally', 'George', 'Jessica']
+activities = ['walking', 'running', 'cycling']
+
+puts sentence(name(names), activity(activities))
