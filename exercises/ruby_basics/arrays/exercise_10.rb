9b5cd9e8f0ad3fd78ad12c9b425458d3b54f006e
Conway <kyle.conway@sykes.com>
2016-08-21 17:46:09 -0700

    Work Sun

---
+++ exercises/ruby_basics/arrays/exercise_10.rb
@@ -0,0 +1,7 @@
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+# array1.each_index do |x|
+#   puts array1[x] == array2[x]
+# end
+puts array1 == array2
---
+++ exercises/ruby_basics/arrays/exercise_7.rb
@@ -0,0 +1,4 @@
+numbers = [5, 9, 21, 26, 39]
+
+divisble_by_three = numbers.select { |number| (number % 3) == 0 }
+p divisble_by_three
