ee77d53e7c77edac956adb49a4adfa4086ca5f32
Conway <kyle.conway@sykes.com>
2016-09-11 16:54:34 -0700

    Work Sun

--- exercises/small_problems/easy_4/exercise_10.rb
+++ exercises/small_problems/easy_4/exercise_10.rb
@@ -1,6 +1,6 @@
 DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-def integer_to_string(number)
+def signed_integer_to_string(number)
   result = ''
   loop do
     number, remainder = number.divmod(10)
@@ -10,6 +10,6 @@ def integer_to_string(number)
   result
 end
 
-p integer_to_string(4321) == '4321'
-p integer_to_string(0) == '0'
-p integer_to_string(5000) == '5000'
+p signed_integer_to_string(4321) == '+4321'
+p signed_integer_to_string(-123) == '-123'
+p signed_integer_to_string(0) == '0'
