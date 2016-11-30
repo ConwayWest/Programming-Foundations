a05489a4dce2602cf2bbd9631e3516a6c1ce39d9
Kyle Conway <kyleconway@rocketmail.com>
2016-11-29 21:38:49 -0500

    Tuesday Night

--- final_redo/lesson_4/tictactoe.rb
+++ final_redo/lesson_4/tictactoe.rb
@@ -1,14 +1,18 @@
-require 'pry'
-
-INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
+  system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is  #{COMPUTER_MARKER}."
   puts ""
   puts "     |     |  "
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -23,6 +27,7 @@ def display_board(brd)
   puts "     |     |  "
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
@@ -46,8 +51,53 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-board = initialize_board
-display_board(board)
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
 
-player_places_piece!(board)
-display_board(board)  
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+  display_board(board)
+
+  loop do
+    player_places_piece!(board)
+    display_board(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    display_board(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing Tic Tac Toe! Good bye!"
